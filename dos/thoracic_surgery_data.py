490a9e3943dad624921ca512e70d7a63c0100c18
Mohamed Eldesouki <disooqi@gmail.com>
2019-10-30 07:34:05 -0400

    Modify the code so that the training score is calculated using the GridSearchCV instead

--- dos/thoracic_surgery_data.py
+++ dos/thoracic_surgery_data.py
@@ -48,8 +48,8 @@ param_grid = {
     'X_transform__num__si__strategy': ['mean', 'median'],
     'mlp__solver': ['sgd', 'adam', 'lbfgs'],
     'mlp__alpha': [1e-1, 1e-3, 1e-5],
-    'mlp__hidden_layer_sizes': [(5, 2), (4, 3), (4, 4), (5, 5)],
-
+    'mlp__hidden_layer_sizes': [(10,), (20,), (5, 2), (4, 3), (4, 4)],
+    'mlp__activation': ['identity', 'logistic', 'tanh', 'relu'],
 }
 
 knn_pipe = Pipeline([
@@ -57,16 +57,15 @@ knn_pipe = Pipeline([
     ('knn', KNeighborsClassifier(n_neighbors=8)),
 ])
 
-knn_pipe.fit(dataset, y)
-print(f'All data score: {knn_pipe.score(dataset, y)}')
-
 knn_param_grid = {
     'X_transform__num__si__strategy': ['mean', 'median'],
     'knn__n_neighbors': range(1, 10),
 }
 
-gs = GridSearchCV(knn_pipe, knn_param_grid, cv=kf)
+gs = GridSearchCV(ml_pipe, param_grid, cv=kf)
 gs.fit(dataset, y)
 print(gs.best_params_)
-print(gs.best_score_)
-print(pd.DataFrame(gs.cv_results_))
+print('The CV best score:', gs.best_score_)
+# print(pd.DataFrame(gs.cv_results_))
+
+print(f'The train set score: {gs.score(dataset, y)} ')
