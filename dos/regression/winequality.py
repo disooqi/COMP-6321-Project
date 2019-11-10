297579bc8e8dc736e28124337a3598d8670b524c
Mohamed Eldesouki <disooqi@gmail.com>
2019-11-09 21:04:55 -0500

    Train both 'MLP' and 'Random Forest' regressors for 'winequality' data

---
+++ dos/regression/winequality.py
@@ -0,0 +1,99 @@
+'''
+https://www.scitepress.org/Papers/2015/55519/55519.pdf
+http://archive.ics.uci.edu/ml/datasets/Wine+Quality
+
+'''
+
+import os
+import pandas as pd
+from scipy.io import arff
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder, OrdinalEncoder, StandardScaler
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.neural_network import MLPRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split, cross_val_score, KFold, GridSearchCV
+from sklearn.metrics import mean_absolute_error
+
+
+dataset_r = pd.read_csv(r'..\..\data\regression\wine-quality\winequality-red.csv', delimiter=';', )  # header=None, usecols=[3,6]
+dataset_w = pd.read_csv(r'..\..\data\regression\wine-quality\winequality-white.csv', delimiter=';')
+# dataset.pop('ID')
+dataset_r['red?'] = True
+dataset_w['red?'] = False
+# print(dataset_r)
+# print(dataset_w)
+
+dataset = pd.concat([dataset_w, dataset_r, ], ignore_index=True)
+y = LabelEncoder().fit_transform(dataset.pop('quality').values)
+
+cat_si_step = ('si', SimpleImputer(strategy='constant', fill_value=-99))  # This is for training
+ohe_step = ('ohe', OneHotEncoder(sparse=False, handle_unknown='ignore'))  # This is for testing
+oe_step = ('le', OrdinalEncoder())
+num_si_step = ('si', SimpleImputer(strategy='median'))
+sc_step = ('sc', StandardScaler())
+
+cat_pipe = Pipeline([cat_si_step, ohe_step])
+num_pipe = Pipeline([num_si_step, sc_step])
+bin_pipe = Pipeline([oe_step])
+
+transformers = [
+    ('cat', cat_pipe, []),
+    ('num', num_pipe, ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides',
+                       'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol']),
+    ('bin', bin_pipe, ['red?']),
+]
+ct = ColumnTransformer(transformers=transformers)
+# X_transformed = ct.fit_transform(dataset)
+# print(X_transformed)
+
+mlp_pipe = Pipeline([
+    ('X_transform', ct),
+    ('mlp', MLPRegressor()),
+])
+
+kf = KFold(n_splits=5, shuffle=True)
+# cv_score = cross_val_score(ml_pipe, dataset, y, cv=kf).mean()
+
+mlp_param_grid = {
+    'X_transform__num__si__strategy': ['mean', 'median'],
+    'mlp__solver': ['sgd', 'adam', 'lbfgs'],
+    'mlp__alpha': [1e-1, 1e-3, 1e-5],
+    'mlp__hidden_layer_sizes': [(10,), (20,), (5, 2), (4, 3), (4, 4)],
+    'mlp__activation': ['identity', 'logistic', 'tanh', 'relu'],
+}
+
+rf_pipe = Pipeline([
+    ('X_transform', ct),
+    ('rf', RandomForestRegressor()),
+])
+
+# knn_pipe.fit(dataset, y)
+# print(f'All data score: {knn_pipe.score(dataset, y)}')
+
+rf_param_grid = {
+    'X_transform__num__si__strategy': ['mean', 'median'],
+    'rf__n_estimators': range(1, 20),
+    # 'rf__criterion': ['mse', 'mae'],
+}
+
+ml_pipe = mlp_pipe
+ml_param_grid = mlp_param_grid
+
+
+gs = GridSearchCV(ml_pipe, ml_param_grid, cv=kf, return_train_score=True, scoring='r2')
+gs.fit(dataset, y)
+
+file_name = os.path.splitext(os.path.basename(__file__))[0]
+with open(f'{file_name}-result.txt', mode='a', encoding='utf-8') as handler:
+    handler.write(f'The CV best neg_mean_absolute_error score: {gs.best_score_:.4f}\n')
+    handler.write(f'The train set neg_mean_absolute_error score: {gs.score(dataset, y):.4f}\n')
+    handler.write(f'{gs.best_params_}\n')
+    handler.write(f'{gs.best_estimator_}\n')
+    # handler.write(f'{gs.grid_scores_}\n')
+    # pd.DataFrame(gs.cv_results_).to_html(f'{file_name}-cv_results_.html')
+    # handler.write(f'{pd.DataFrame(gs.cv_results_)}\n')
+    handler.write('#'*120)
+    handler.write('\n')
+
