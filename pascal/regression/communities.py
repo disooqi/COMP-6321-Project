744a7ebb68015407fbe98052f01bce25bdb4feea
Ufuoma Ubor <foskynew@gmail.com>
2019-12-06 20:43:45 -0500

    final regression models

--- pascal/regression/communities.py
+++ pascal/regression/communities.py
@@ -1,10 +1,12 @@
 import os
 import pandas as pd
-import numpy as np
 import sklearn
+from scipy.io import arff
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.feature_selection import chi2
+from sklearn.feature_selection import SelectFromModel, SelectKBest
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error, mean_squared_error
 from sklearn.multioutput import MultiOutputRegressor
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder, OrdinalEncoder, StandardScaler
 from sklearn.impute import SimpleImputer
@@ -12,82 +14,131 @@ from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split, cross_val_score, KFold, GridSearchCV
 from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C, WhiteKernel, DotProduct
+from sklearn.svm import LinearSVC
 
+#  data/regression/communities-and-crime-data-set/communities.names
+#  data/regression/communities-and-crime-data-set/communities.data
+data, meta = arff.loadarff(r'data/regression/communities-and-crime-data-set/communities.arff')
+dataset = pd.DataFrame(data)
 
-def preprocessFeatures(X, index_, auto=False):
-    """ The following function is used to convert categorical data into numerical data
-    @X This is the feature vector of type DataFrame
-    @index_ This is the list of columns that need to be converted to numerical data
-    @auto To auto detect the categorical columns and convert them to numerical data
-    @return DataFrame
-    example: Suppose a column has two types of categorical data which includes 'a' and 'b'
-    ----col----
-        a
-        a
-        b
-    This will be transformed to
-    ----a----b----
-        1    0
-        1    0
-        0    1 """
+''' # Dropping non-predictive features
+-- state: US state (by number) - not counted as predictive above, but if considered, should be considered nominal (nominal)
+-- county: numeric code for county - not predictive, and many missing values (numeric)
+-- community: numeric code for community - not predictive and many missing values (numeric)
+-- communityname: community name - not predictive - for information only (string)
+-- fold: fold number for non-random 10 fold cross validation, potentially useful for debugging, paired tests - not predictive (numeric)
+'''
+dataset.drop(['state', 'county', 'community', 'communityname', 'fold'], axis=1, inplace=True)
+y = dataset.pop('ViolentCrimesPerPop').values
+dataset.drop(['LemasSwornFT', 'LemasSwFTPerPop', 'LemasSwFTFieldOps', 'LemasSwFTFieldPerPop', 'LemasTotalReq',
+              'LemasTotReqPerPop', 'PolicReqPerOffic', 'PolicPerPop', 'RacialMatchCommPol', 'PctPolicWhite',
+              'PctPolicBlack', 'PctPolicHisp', 'PctPolicAsian', 'PctPolicMinor', 'OfficAssgnDrugUnits',
+              'NumKindsDrugsSeiz', 'PolicAveOTWorked', 'PolicCars', 'PolicOperBudg', 'LemasPctPolicOnPatr',
+              'LemasGangUnitDeploy', 'PolicBudgPerPop'], axis=1, inplace=True)
 
-    output = pd.DataFrame(index=X.index)
+## Search for
+# what should happen when most of the data in column is missing? [options: new value not in the column, drop the column]
+# what do we do in case of many duplicates in the data? [drop, keep]
+'''
+OtherPerCap  1
+LemasSwornFT  1675
+LemasSwFTPerPop  1675
+LemasSwFTFieldOps  1675
+LemasSwFTFieldPerPop  1675
+LemasTotalReq  1675
+LemasTotReqPerPop  1675
+PolicReqPerOffic  1675
+PolicPerPop  1675
+RacialMatchCommPol  1675
+PctPolicWhite  1675
+PctPolicBlack  1675
+PctPolicHisp  1675
+PctPolicAsian  1675
+PctPolicMinor  1675
+OfficAssgnDrugUnits  1675
+NumKindsDrugsSeiz  1675
+PolicAveOTWorked  1675
+PolicCars  1675
+PolicOperBudg  1675
+LemasPctPolicOnPatr  1675
+LemasGangUnitDeploy  1675
+PolicBudgPerPop  1675
+'''
+cat_si_step = ('si', SimpleImputer(strategy='constant', fill_value=-99))  # This is for training
+ohe_step = ('ohe', OneHotEncoder(sparse=False, handle_unknown='ignore'))  # This is for testing
+oe_step = ('le', OrdinalEncoder())
+num_si_step = ('si', SimpleImputer(strategy='constant'))
+sc_step = ('sc', StandardScaler())
 
-    if (auto is True):
-        for col, col_data in X.iteritems():
-            if (col_data.dtype == object):
-                col_data = pd.get_dummies(col_data)
-            output = output.join(col_data, how='left', lsuffix='_left', rsuffix='_right')
+cat_pipe = Pipeline([cat_si_step, ohe_step])
+num_pipe = Pipeline([num_si_step, sc_step])
+bin_pipe = Pipeline([oe_step])
 
-    else:
-        for col, col_data in X.iteritems():
-            if (col in index_):
-                col_data = pd.get_dummies(col_data)
-            output = output.join(col_data, how='left', lsuffix='_left', rsuffix='_right')
+transformers = [
+    ('cat', cat_pipe, []),
+    ('num', num_pipe, list(dataset.columns)),
+    ('bin', bin_pipe, []),
+]
+ct = ColumnTransformer(transformers=transformers)
+# X_transformed = ct.fit_transform(dataset)
+# print(X_transformed.shape)
 
-    return output
+linear_r_pipe = Pipeline([
+    ('X_transform', ct),
+    ('linear_r', LinearRegression(fit_intercept=True)),
+])
 
+kf = KFold(n_splits=5, shuffle=True)
+# cv_score = cross_val_score(ml_pipe, dataset, y, cv=kf).mean()
 
-path = 'data/regression/communities-and-crime-data-set/communities.data'
+linear_r_param_grid = {
+    'X_transform__num__si__strategy': ['mean', 'median'],
+    # 'mlp__solver': ['sgd', 'adam', 'lbfgs'],
+    # 'mlp__alpha': [1e-1, 1e-3, 1e-5],
+    # 'mlp__hidden_layer_sizes': [(10,), (20,), (5, 2), (4, 3), (4, 4)],
+    # 'mlp__activation': ['identity', 'logistic', 'tanh', 'relu'],
+}
+k1 = 1 ** 2 * RBF(length_scale=100) + WhiteKernel(noise_level=1)
+# k2 = 1.0 * RBF(length_scale=1.0, length_scale_bounds=(1e-2, 1e3)) \
+#     + WhiteKernel(noise_level=1e-5, noise_level_bounds=(1e-10, 1e+1))
+# k3 = DotProduct(sigma_0=1) + WhiteKernel(noise_level=1)
+# k4 = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
+gp_pipe = Pipeline([
+    ('X_transform', ct),
+    ('gp', GaussianProcessRegressor(kernel=k1)),
+])
 
-dataset = pd.read_csv(path, header=None).replace('?', np.NaN)
-dataset = dataset.convert_objects(convert_numeric=True)
+gp_param_grid = {
+    'X_transform__num__si__strategy': ['median'],
+    # 'gp__alpha': [1e-01, 1e-03, 1e-05],
+    # 'gp__kernel': [k2]
+    # 'rf__n_estimators': range(1, 20),
+    # 'rf__criterion': ['mse', 'mae'],
+}
 
-for i in dataset.columns:
-    if dataset[i].dtype in [np.float64, np.int64]:
-        dataset[i] = dataset[i].fillna(dataset[i].mean())
+scoring = 'r2'
+gs_linear_r = GridSearchCV(linear_r_pipe, linear_r_param_grid, cv=kf, scoring=scoring)
+gs_linear_r.fit(dataset, y)
+print("##################### Linear Regression for Sgemm Dataset ############################")
+print('CV Score', gs_linear_r.best_score_)
+print('Training accuracy', gs_linear_r.score(dataset, y))
+print("##################### GaussianProcessRegressor for Communities Dataset #####################")
+gs_gp = GridSearchCV(gp_pipe, gp_param_grid, cv=kf, scoring=scoring)
+gs_gp.fit(dataset, y)
+print('CV Score', gs_gp.best_score_)
+print('Training accuracy', gs_gp.score(dataset, y))
 
-dataset = preprocessFeatures(dataset, [], True).to_numpy()
-
-X_original = dataset[:, :dataset.shape[1] - 1]
-y = dataset[:, dataset.shape[1] - 1]
-
-X_train, X_test, y_train, y_test = train_test_split(X_original, y, test_size=0.25, random_state=0, shuffle=True)
-
-support_vector = LinearRegression(X_train, y_train)
-# support_vector = SVR().fit(X_train, y_train)
-
-print('#' * 10, 'communities', '#' * 10)
-
-print('\n')
-
-print('support vector machine (r2 on training): ', support_vector.score(X_train, y_train))
-print('support vector machine (MAE on train): ', mean_absolute_error(support_vector.predict(X_train), y_train))
-print('support vector machine (MSE on train): ', mean_squared_error(support_vector.predict(X_train), y_train))
-print('support vector machine (MAE on test): ', mean_absolute_error(support_vector.predict(X_test), y_test))
-print('support vector machine (MSE on test): ', mean_squared_error(support_vector.predict(X_test), y_test))
-
-print('\n')
-
-# DecisionTreeRegressor
-
-tree = GaussianProcessRegressor(X_train, y_train)
-print('\n')
-
-print('DecisionTreeRegressor (r2 on training): ', tree.score(X_train, y_train))
-print('DecisionTreeRegressor (MAE on train): ', mean_absolute_error(tree.predict(X_train), y_train))
-print('DecisionTreeRegressor (MSE on train): ', mean_squared_error(tree.predict(X_train), y_train))
-print('DecisionTreeRegressor (MAE on test): ', mean_absolute_error(tree.predict(X_test), y_test))
-print('DecisionTreeRegressor (MSE on test): ', mean_squared_error(tree.predict(X_test), y_test))
-
-print('\n')
+# file_name = os.path.splitext(os.path.basename(__file__))[0]
+# with open(f'{file_name}-result.txt', mode='a', encoding='utf-8') as handler:
+#     handler.write(f'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ LINEAR REGRESSION FOR STUDENT_PERFORMANCE DATA @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n')
+#     handler.write(f'The CV best {scoring} score: {gs_linear_r.best_score_:.4f}\n')
+#     handler.write(f'The train set {scoring} score: {gs_linear_r.score(dataset, y):.4f}\n')
+#     handler.write(f'{gs_linear_r.best_params_}\n')
+#     handler.write(f'{gs_linear_r.best_estimator_}\n')
+#     handler.write(f'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ GAUSSIAN PROCESS FOR STUDENT_PERFORMANCE DATA @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n')
+#     handler.write(f'The CV best {scoring} score: {gs_gp.best_score_:.4f}\n')
+#     handler.write(f'The train set {scoring} score: {gs_gp.score(dataset, y):.4f}\n')
+#     handler.write(f'{gs_gp.best_params_}\n')
+#     handler.write(f'{gs_gp.best_estimator_}\n')
+#     handler.write('#' * 120)
+#     handler.write('\n')
